8269cec8cbd945b8b7360485f62b899d45752954
Tudor Tocan <tudor.tocan@gmail.com>
2022-07-02 10:17:57 +0100

    ddinamically rendering home element on load, when choosing home in navbar

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,20 @@ const homeButton = document.getElementById("homeButton");
 const menuButton = document.getElementById("menuButton");
 const container = document.getElementById("container");
 
-// secont bit - logic
-const returnHome = function () {
-  console.log("Home");
+// second bit - logic
+const renderHome = function () {
+  // create html tag
+  const welcomeHeader = document.createElement("h3");
+  // set the inner text of the html tag
+  welcomeHeader.innerHTML = "Welcome to our restaurant";
+  // add class for created html tag/element
+  welcomeHeader.classList.add("welcome-message");
+
+  container.appendChild(welcomeHeader);
+  // container.appendChild(openingTimes);
 };
+
+window.onload = renderHome();
 
 const showMenu = function () {
   // empty container contents
@@ -14,5 +24,5 @@ const showMenu = function () {
 };
 
 // third bit - add event listeners
-homeButton.addEventListener("click", returnHome);
+homeButton.addEventListener("click", renderHome);
 menuButton.addEventListener("click", showMenu);
